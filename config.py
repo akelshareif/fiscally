eddacb947fc53e438d88b43077caf50557da2247
Abdelkareem ElShareif <abdelkareemelshareif@abdelkareemsmbp.lan>
2020-08-10 01:30:29 -0400

    troubleshooting heroku deployment

--- config.py
+++ config.py
@@ -19,7 +19,8 @@ class ProdConfig(Config):
 
     DEBUG = False
     TESTING = False
-    SQLALCHEMY_DATABASE_URI = environ.get('PROD_DATABASE_URI')
+    SECRET_KEY = environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = False
 
