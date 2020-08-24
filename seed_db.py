a021cff68229298e292c7161f3b302adb895df0c
Abdelkareem ElShareif <abdelkareemelshareif@abdelkareemsmbp.lan>
2020-08-23 21:16:20 -0400

    deleted db seed file

--- seed_db.py
+++
@@ -1,9 +0,0 @@
-from application import db  # Models go here
-from application import app
-
-# Create tables
-db.drop_all()
-db.create_all()
-
-# Replace Model with a Model for every model to ensure all tables are empty
-Model.query.delete()
